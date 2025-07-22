050ab0b296f7551c068b69fa29ae347b69491060
vianaIgor <igoorviannax@gmail.com>
2025-07-22 08:37:01 -0300

    upgrade no projeto

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let result = document.querySelector(".result")
 let mypoints = document.querySelector(".my-points")
 let machinepoints = document.querySelector(".machine-points")
+let resultMachine = document.querySelector(".result-machine")
+
 
 let myPointsNumber = 0
 let machinePointsNumber = 0
@@ -22,6 +24,8 @@ const playTheGame = (human, machine) => {
 
     console.log(`Humano:  ${human} Machine: ${machine}`)
 
+    resultMachine.innerHTML = `O JavaScript escolheu: ${machine}`
+
     if (human === machine) {
         result.innerHTML = "deu empate"
     }else if ( human === 'rock' && machine === 'scissors' ||
@@ -37,6 +41,15 @@ const playTheGame = (human, machine) => {
         machinePointsNumber++
         machinepoints.innerHTML = machinePointsNumber
     }
+}
+
+const resetGame = () => {
+    myPointsNumber = 0
+    machinePointsNumber = 0
+    mypoints.innerHTML = 0
+    machinepoints.innerHTML = 0
+    result.innerHTML = ''
+    resultMachine.innerHTML = ''
 }
 
 
